49b1a158ec9d9feeaea59b359e85b85ffa23be79
John Clayton <johnc@filewave.com>
2017-01-01 22:13:20 +0100

    Only attempt to load config if the file exists.

--- src/server/electric/app.py
+++ src/server/electric/app.py
@@ -1,8 +1,10 @@
 from flask import Flask
+import os
 
 class AppInterface(object):
     def __init__(self):
         self.app = Flask(__name__, instance_path='/etc')
-        self.app.config.from_pyfile('/etc/electric.cfg')
+        if os.path.exists('/etc/electric.cfg'):
+            self.app.config.from_pyfile('/etc/electric.cfg')
 
 
