bb1a8bf86b32621a44105cb120eb30b72de3af41
Neil Clayton <neil@shinywhitebox.com>
2017-01-03 11:15:31 +1300

    - Add CORS to server, for cross domain ajax

--- src/server/electric/app.py
+++ src/server/electric/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 import os
 
 class AppInterface(object):
@@ -6,5 +7,6 @@ class AppInterface(object):
         self.app = Flask(__name__, instance_path='/etc')
         if os.path.exists('/etc/electric.cfg'):
             self.app.config.from_pyfile('/etc/electric.cfg')
+        self.cors_app = CORS(self.app)
 
 
