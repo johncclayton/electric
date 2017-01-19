b217a4095b028c9090c1794fd5b05647eecaed0f
John Clayton <johnc@filewave.com>
2017-01-19 14:00:37 +0100

    LF endings for script.

--- run_pi_container_dev.sh
+++ run_pi_container_dev.sh
@@ -1,3 +1,2 @@
 #@IgnoreInspection BashAddShebang
 docker run --restart=always -d --name electric-pi --privileged -v /dev/bus/usb:/dev/bus/usb -v $PWD/src/server:/www -p 5000:5000 scornflake/electric-pi
-
