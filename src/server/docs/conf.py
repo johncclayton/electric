edb59e2026512953dbdb3f14eb4c38da0ab5553f
John Clayton <john_clayton@me.com>
2017-01-30 21:32:21 +0000

    trying a different theme

--- src/server/docs/conf.py
+++ src/server/docs/conf.py
@@ -16,8 +16,10 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
+import os
+import sys
+import sphinx_rtd_theme
+
 # sys.path.insert(0, os.path.abspath('.'))
 
 
@@ -84,7 +86,8 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
