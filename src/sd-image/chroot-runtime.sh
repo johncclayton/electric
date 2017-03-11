0a51abb093a344c8f28a3a6bea1eeecd218b4184
John Clayton <johnc@filewave.com>
2017-03-11 17:38:42 +0100

    adjust apt-get requirements

--- src/sd-image/chroot-runtime.sh
+++ src/sd-image/chroot-runtime.sh
@@ -5,7 +5,7 @@ echo "# sometihng amazing" > /etc/ld.so.preload
 echo 'SUBSYSTEMS=="usb", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="5751", MODE:="0666"' > /etc/udev/rules.d/10-icharger.rules
 
 # DO NOT do apt-get update/upgrade - this causes the sd-card to NOT BOOT
-apt-get -y install python-pip python-dev ipython
+apt-get -y install python-pip python-dev ipython linux-headers py-pip gcc musl-dev cython-dev
 apt-get -y install bluetooth libbluetooth-dev hostapd dnsmasq
 pip install pybluez
 pip install -r /home/pi/requirements.txt
