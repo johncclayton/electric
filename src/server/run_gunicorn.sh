41952338afce24856f43ad1db60cfb3d3b9b5ced
John Clayton <johnc@filewave.com>
2017-07-24 15:21:40 +0200

    Dug. gunicorn runs its own --reload option so I don't need watchmedo on this script.

--- src/server/run_gunicorn.sh
+++ src/server/run_gunicorn.sh
@@ -2,4 +2,4 @@
 BASHSOURCE=$0
 DIR="$( cd "$( dirname "$BASHSOURCE" )" && pwd )"
 export PYTHONPATH=$DIR
-watchmedo auto-restart -p "*.py;" --recursive gunicorn --bind=0.0.0.0:5000 --reload --workers=10 --backlog=300 "electric.wsgi:application"
+gunicorn --bind=0.0.0.0:5000 --reload --workers=10 --backlog=300 "electric.wsgi:application"
