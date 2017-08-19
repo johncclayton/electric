d9a30f3a47329cd9ee6a8627bdecf4a4afa302d8
John Clayton <johnc@filewave.com>
2017-08-19 17:05:36 +0200

    Add exec script for plain old Hypriot images.

---
+++ install_docker.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# go get the docker-compose.yml file
+curl --remote-name --location https://raw.githubusercontent.com/johncclayton/electric/master/docker-compose.yml
+
+# and the udev rules?
+if [ ! -d "/etc/udev/rules.d/10-icharger.rules" ]; then
+    curl --remote-name --location https://raw.githubusercontent.com/johncclayton/electric/master/src/server/scripts/10-icharger.rules
+    mv 10-icharger.rules /etc/udev/rules.d/ && udevadm control --reload
+fi
+
+echo To run this, set DOCKER_TAG to the version you want to run and execute docker-compose up -d
