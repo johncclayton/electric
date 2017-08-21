763272856dfc3d0483fbc029bb8a8c14584e3997
Neil Clayton <neil@shinywhitebox.com>
2017-08-21 16:07:32 +1200

    you can now re-deploy, blowing away existing docker containers with guns blazing. π

--- install-on-rpi3.sh
+++ install-on-rpi3.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+shutdown_existing() {
+    # Is it running?
+    if [ "$(docker ps -q -f name=$1)" ]; then
+        echo "$1 is running ... stopping it"
+        docker stop $1
+    fi
+    if [ "$(docker ps -aq -f name=$1)" ]; then
+        echo "Removing container $1..."
+        docker rm $1
+    fi
+}
+
 set -e
 
 # go get the docker-compose.yml file
@@ -14,8 +26,25 @@ if [ ! -d "/etc/udev/rules.d/10-icharger.rules" ]; then
     mv 10-icharger.rules /etc/udev/rules.d/ && udevadm control --reload
 fi
 
+
+
 VERSION_NUM=`python get-latest-build-number.py`
 echo "Latest version is: $VERSION_NUM"
-echo Running with DOCKER_TAG=$VERSION_NUM, and execute docker-compose up -d
 
+if [ -f 'LAST_DEPLOY' ]; then
+    LAST_DEPLOY=`cat LAST_DEPLOY`
+    if [ "$LAST_DEPLOY" -eq "$VERSION_NUM" ]; then
+        echo "Up to date. No need to redeploy"
+        exit 0
+    fi
+    echo "Deployed is version $LAST_DEPLOY... going to upgrade"
+fi
+
+# If we already have containers, we need to remove them.
+shutdown_existing docker-ui
+shutdown_existing electric-web
+shutdown_existing electric-worker
+
+echo Running with DOCKER_TAG=$VERSION_NUM, and execute docker-compose up -d
 DOCKER_TAG=$VERSION_NUM docker-compose up -d
+echo >LAST_DEPLOY $VERSION_NUM
