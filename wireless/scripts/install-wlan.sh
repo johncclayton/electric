fa89aa6b15b0754bd1cd2bf6b670f78a053edae6
Neil Clayton <neil@shinywhitebox.com>
2017-10-26 11:52:06 +1300

    Fixing wireless script

--- wireless/scripts/install-wlan.sh
+++ wireless/scripts/install-wlan.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-set -e
-
 # lets just say we're gonna install EVERYTHING here
 INSTALL_ROOT=/opt
 
@@ -36,13 +34,18 @@ echo Installing files into /etc...
 cp -avR ${TEMP}/etc/* /etc
 
 # do the iw dev wlan0 add... etc, if the interface wlan1 doesn't already exist.
-# TODO: I don't think this condition fires. needs more work.
-# TODO: idea is that if wlan1 isn't present, we add it. Seems to get skipped?
 HAVE_WLAN1=$(iw dev | grep 'wlan1')
-echo "Check value: ${HAVE_WLAN1}x"
 if [ "${HAVE_WLAN1}x" == "x" ]; then
     echo "Adding wlan1 AP interface..."
-    /opt/wireless/scripts/start-wlan1.sh
+
+    # Add the wlan1 interface
+    iw dev wlan0 interface add wlan1 type __ap
+
+    # Enable IP forwarding and masq.
+    echo 1 >/proc/sys/net/ipv4/ip_forward
+    iptables -t nat -A POSTROUTING -o wlan0 -j MASQUERADE
+    iptables -A FORWARD -i wlan0 -o wlan1 -m state --state RELATED,ESTABLISHED -j ACCEPT
+    iptables -A FORWARD -i wlan1 -o wlan0 -j ACCEPT
 fi
 
 # Fix the WLAN0 ssid/password
