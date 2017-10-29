7d46ab0a56c68438f314f3de0be5b774e6a720b2
John Clayton <johnc@filewave.com>
2017-10-29 22:42:31 +0100

    actually run the install

--- wireless/scripts/install-wlan.sh
+++ wireless/scripts/install-wlan.sh
@@ -8,15 +8,17 @@ if [ "$EUID" -ne 0 ]; then
   exit -2
 fi
 
-if [ ! -f /proc/device-tree/model ]; then
-    echo "You're not running this on a pi3, are you?"
-    exit -1
-fi
+if [ ! -z ${SKIP_PI3_CHECK+x} ]; then
+    if [ ! -f /proc/device-tree/model ]; then
+        echo "You're not running this on a pi3, are you?"
+        exit -1
+    fi
 
-PI_MODEL=$(cat /proc/device-tree/model | awk '{print $1 $2 $3}')
-if [ ${PI_MODEL} != 'RaspberryPi3' ]; then
-    echo "This computer doesn't appear to be a pi3"
-    exit -1
+    PI_MODEL=$(cat /proc/device-tree/model | awk '{print $1 $2 $3}')
+    if [ ${PI_MODEL} != 'RaspberryPi3' ]; then
+        echo "This computer doesn't appear to be a pi3"
+        exit -1
+    fi
 fi
 
 TEMP=${INSTALL_ROOT}/wireless
