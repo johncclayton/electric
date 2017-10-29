c088410b2903005bb08f1e00ea981e09e831c52f
John Clayton <johnc@filewave.com>
2017-10-29 12:56:56 +0100

    First shot.

--- src/sd-image/chroot-runtime.sh
+++ src/sd-image/chroot-runtime.sh
@@ -6,42 +6,31 @@ cp /etc/ld.so.preload /etc/ld.so.preload-backup
 echo "# this stops the runtime from aborting entirely - its undone at the end" > /etc/ld.so.preload
 
 # this ensures that udev will recognize the iCharger for non-root users when plugged in.
-echo 'SUBSYSTEMS=="usb", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="5751", MODE:="0666"' > /etc/udev/rules.d/10-icharger.rules
+mv /home/pi/10-icharger.rules /etc/udev/rules.d/10-icharger.rules
 
 # DO NOT do apt-get upgrade - this causes the sd-card to NOT BOOT
 apt-get -y update
-apt-get -y install python-dev python-setuptools python-pip hostapd dnsmasq libbluetooth-dev bluez
+apt-get -y install python-dev python-setuptools python-pip hostapd dnsmasq gawk avahi-daemon libbluetooth-dev bluez
 
 /usr/bin/pip install -r /home/pi/status/requirements.txt
 
-curl -sSL https://get.docker.com | sh
+# curl -sSL https://get.docker.com | sh
 
 usermod -aG docker pi
 
 # compile the enumeration_interfaces.c code for raspberry pi
 pushd . && cd /home/pi/status && gcc -o enumerate_interfaces enumerate_interfaces.c && popd
 
-sudo mv /etc/dnsmasq.conf /etc/dnsmasq.conf.old
-sudo mv /home/pi/dnsmasq.conf /etc/
-
-sudo sed -i "s,DAEMON_CONF=,DAEMON_CONF=/etc/hostapd/hostapd.conf,g" /etc/init.d/hostapd
-sudo sed -i "s/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g" /etc/sysctl.conf
-
-sudo mkdir /etc/systemd/system/dnsmasq.service.d
-sudo mv /home/pi/dnsmasq-pre.conf /etc/systemd/system/dnsmasq.service.d/dnsmasq-pre.conf
-sudo mv /home/pi/hostapd.conf /etc/hostapd/
-
-systemctl enable dnsmasq.service
-systemctl enable electric-pi.service
-systemctl enable electric-pi-status.service
+# systemctl enable electric-pi-status.service
 
 # owned by the right user
 sudo chown -R pi:users /home/pi
 
 # ensure SSH is enabled
 touch /boot/ssh
+
 # make sure we don't have predictable network names enabled - horrible idea.
-echo ' net.ifnames=0 ' >> /boot/cmdline.txt
+# echo ' net.ifnames=0 ' >> /boot/cmdline.txt
 
 rm /etc/ld.so.preload
 mv /etc/ld.so.preload-backup /etc/ld.so.preload
--- src/sd-image/create-image.sh
+++ src/sd-image/create-image.sh
@@ -5,15 +5,13 @@ set -x
 
 FROM="$1"
 TO="$2"
+
 PIIMG=`which piimg`
 MNT="/mnt"
 QEMU_ARM="/usr/bin/qemu-arm-static"
+
 DOCKER_IMAGE_WEB="johncclayton/electric-pi-web"
 DOCKER_IMAGE_WORKER="johncclayton/electric-pi-worker"
-APNAME="ELECTRIC-PI"
-APPWD="pa55word"
-WIFINAME=""
-WIFIPWD=""
 
 if [ -z "$FROM" -o -z "$TO" ]; then
 	echo "Use create-image.sh <from> <to>"
@@ -59,49 +57,51 @@ if [ -f ./.config ]; then
 	. ./.config
 fi
 
-echo "Access Point Name: $APNAME"
-echo "Access Point Pass: $APPWD"
-echo "WiFi Name        : $WIFINAME"
-echo "WiFi Pass        : $WIFIPWD"
-
-if [ -z "$APNAME" -o -z "$APPWD" -o -z "$WIFINAME" -o -z "$WIFIPWD" ]; then
-	echo "Fail - APNAME/APPWD/WIFINAME and WIFIPWD all need to be filled in, put these into the .config file"
-	exit 6
-fi
+# get the script to fetch the latest build # from travis
+curl --remote-name --location https://raw.githubusercontent.com/johncclayton/electric/master/get-latest-build-number.py
+VERSION_NUM=`python get-latest-build-number.py`
+echo "Latest version is: $VERSION_NUM"
+echo >LAST_DEPLOY $VERSION_NUM
 
 # pull docker image and save it as a file...
-docker pull "$DOCKER_IMAGE_WEB"
-docker pull "$DOCKER_IMAGE_WORKER"
+docker pull "$DOCKER_IMAGE_WEB:$VERSION_NUM"
+docker pull "$DOCKER_IMAGE_WORKER:$VERSION_NUM"
 
 # copy source -> dest
 cp "$FROM" "$TO" 
+
 sudo $PIIMG mount "$TO" "$MNT"
 
 sudo cp "$QEMU_ARM" "$MNT/usr/bin/"
-sudo cp scripts/* "$MNT/home/pi/"
+sudo cp -r ../../wireless/* "$MNT/home/pi/"
+
+# fetch the iCharger rules
+curl --remote-name --location https://raw.githubusercontent.com/johncclayton/electric/master/src/server/scripts/10-icharger.rules
+mv 10-icharger.rules "$MNT/home/pi/"
 
 sudo touch "$MNT/home/pi/docker_image_web.tar.gz" && sudo chmod 777 "$MNT/home/pi/docker_image_web.tar.gz"
 sudo touch "$MNT/home/pi/docker_image_worker.tar.gz" && sudo chmod 777 "$MNT/home/pi/docker_image_worker.tar.gz"
+docker image save "$DOCKER_IMAGE_WEB:$VERSION_NUM" | gzip > "$MNT/home/pi/docker_image_web.tar.gz"
+docker image save "$DOCKER_IMAGE_WORKER:$VERSION_NUM" | gzip > "$MNT/home/pi/docker_image_worker.tar.gz"
 
-docker image save "$DOCKER_IMAGE_WEB" | gzip > "$MNT/home/pi/docker_image_web.tar.gz"
-docker image save "$DOCKER_IMAGE_WORKER" | gzip > "$MNT/home/pi/docker_image_worker.tar.gz"
-
-sudo cp scripts/rc.local "$MNT/etc/rc.local"
-sudo cp scripts/electric-pi.service "$MNT/etc/systemd/system/"
-sudo cp scripts/electric-pi-status.service "$MNT/etc/systemd/system/"
-sudo cp scripts/wpa_supplicant.conf "$MNT/etc/wpa_supplicant/"
-sudo cp scripts/network_interfaces "$MNT/etc/network/interfaces"
-sudo cp scripts/hostapdstart "$MNT/usr/local/bin/hostapdstart"
-sudo cp scripts/90-wireless.rules "$MNT/etc/udev/rules.d/"
+# sudo cp scripts/electric-pi-status.service "$MNT/etc/systemd/system/"
 
 sudo cp -r ../status "$MNT/home/pi/"
 sudo cp ../../docker-compose.yml "$MNT/home/pi/"
 
-# set up the Access Point name/password defaults.
-sudo sed -i "s/APNAME/$APNAME/g" "$MNT/home/pi/hostapd.conf"
-sudo sed -i "s/APPWD/$APPWD/g" "$MNT/home/pi/hostapd.conf"
-sudo sed -i "s/WIFINAME/$WIFINAME/g" "$MNT/etc/wpa_supplicant/wpa_supplicant.conf"
-sudo sed -i "s/WIFIPWD/$WIFIPWD/g" "$MNT/etc/wpa_supplicant/wpa_supplicant.conf"
+# make up a script that contains the version number in it, but its done
+# in a way that'll make upgrades later easier - the script knows to pull the 
+# version number from a file - so upgrade process can simply keep this file up to date
+
+echo > "$MNT/home/pi/compose-command.sh" <<--MYDOCKERSCRIPT--
+#!/usr/bin/env bash
+export DOCKER_TAG=`cat LAST_DEPLOY`
+docker-compose up -d
+ --MYDOCKERSCRIPT--
+
+chmod 777 "$MNT/home/pi/compose-command.sh" 
+
+find "${MNT}/home/pi/*.sh" -type f | xargs chmod +x
 
 sudo chroot "$MNT" < ./chroot-runtime.sh
 RES=$?
@@ -111,11 +111,6 @@ if [ -d "$HOME/Dropbox/Public" -a "$RES" -eq 0 ]; then
 	cp "$TO" "$HOME/Dropbox/Public/"
 else
 	echo "$TO not moved, there was a problem"
-fi
-
-
-if [ -f "/media/john.img" ]; then
-    cp "$TO" "/media/"
 fi
 
 exit 0
