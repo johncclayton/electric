6ecfd72ab86f05fca5751fce62e0903c7e95bcf1
Neil Clayton <neil@shinywhitebox.com>
2017-11-04 11:20:25 +1300

    - dont auto load the config page - shutdown hostapd/wlan0 before rewriting wpa_supplicant, force hostapd back up after, regardless - make system check for wlan1 on boot

--- src/status/scripts/sudo_set_ssid.sh
+++ src/status/scripts/sudo_set_ssid.sh
@@ -11,7 +11,14 @@ if [ -z "$WIFINAME" -o -z "$WIFIPWD" ]; then
     exit 1
 fi
 
+/etc/init.d/hostapd stop
+
+/sbin/ifdown wlan0
+
 # sets the current configured SSID/PSK value in the wpa_supplicant.conf file
 /usr/bin/wpa_passphrase "$WIFINAME" "$WIFIPWD" > /etc/wpa_supplicant/wpa_supplicant.conf
-/sbin/ifdown wlan0 
+
 /sbin/ifup wlan0
+
+# Bring up hostapd again, even if bringing up wlan0 failed
+/etc/init.d/hostapd start
--- wireless/scripts/functions.sh
+++ wireless/scripts/functions.sh
@@ -1,5 +1,24 @@
 #!/usr/bin/env bash
 
+add_wlan1_if_not_there() {
+    # Add the wlan1 interface if it's not there
+    HAVE_WLAN1=$(iw dev | grep 'wlan1')
+    if [ "${HAVE_WLAN1}x" = "x" ]; then
+        echo "Adding in wlan1 AP interface..."
+        iw dev wlan0 interface add wlan1 type __ap
+    fi
+    return 0;
+}
+
+check_firewall_forwarding_rules() {
+    HAVE_IP_RULES=$(iptables -S -t nat | grep wlan0)
+    if [ "$HAVE_IP_RULES" != "-A POSTROUTING -o wlan0 -j MASQUERADE" ]; then
+        echo "Adding in IP masquerade rules"
+        iptables -t nat -A POSTROUTING -o wlan0 -j MASQUERADE
+        iptables -A FORWARD -i wlan0 -o wlan1 -m state --state RELATED,ESTABLISHED -j ACCEPT
+        iptables -A FORWARD -i wlan1 -o wlan0 -j ACCEPT
+    fi
+}
 
 ask_question() {
     read -p "$1 [y/N]  " -n 1 -r
