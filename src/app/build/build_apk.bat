f12f065db1f3ab675be09bd6ca662b827f90e925
John Clayton <johnc@filewave.com>
2017-11-04 08:41:26 +0100

    for 1.0.2 build - everything is deployed as 494 already

--- src/app/build/build_apk.bat
+++ src/app/build/build_apk.bat
@@ -1,3 +1,4 @@
-REM ionic cordova build --prod --release android
+ionic cordova build --prod --release android
 "%JAVA_HOME%\bin\jarsigner" -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore electric-release.keystore platforms\android\build\outputs\apk\android-release-unsigned.apk electric-charger
+DEL platforms\android\build\outputs\apk\android-release-signed.apk
 "%ANDROID_HOME%\build-tools\26.0.0\zipalign" -v 4 platforms\android\build\outputs\apk\android-release-unsigned.apk platforms\android\build\outputs\apk\android-release-signed.apk
