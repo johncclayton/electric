9d298375d1f697088bcd1553382f24732d7c4b7c
John Clayton <johnc@filewave.com>
2017-11-05 21:35:12 +0100

    Did some things.

--- travis_webhook_to_build_sd_image.sh
+++ travis_webhook_to_build_sd_image.sh
@@ -1,1 +1,3 @@
 echo $YAY
+openssl aes-256-cbc -K $encrypted_00ede72e53d7_key -iv $encrypted_00ede72e53d7_iv -in travis_rsa.pem.enc -out ./travis_rsa.pem -d 
+"cd electric/src/sd-image && git pull && ./create-image.sh" | ssh -i ./travis_rsa.pem builder@coderage-software.com /bin/bash
