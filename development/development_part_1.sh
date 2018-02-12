6abb15ca57651ea2649a45f7f2930d587ea3ac52
Neil Clayton <neil@shinywhitebox.com>
2018-02-12 17:04:47 +1300

    - Make sure a dev user has access to GPIO as well

--- development/development_part_1.sh
+++ development/development_part_1.sh
@@ -7,6 +7,12 @@ wget https://bootstrap.pypa.io/get-pip.py
 sudo python get-pip.py
 sudo pip install virtualenv virtualenvwrapper
 
+# So that we can access GPIO of the pi3
+sudo groupadd gpio
+sudo adduser pirate gpio
+sudo chown root.gpio /dev/gpiomem
+sudo chmod g+rw /dev/gpiomem
+
 echo 'source /usr/local/bin/virtualenvwrapper.sh' >> ~/.bashrc
 echo "***************************************"
 echo "Part 1 done. Please re login to the pi and run development_part_2.sh"
