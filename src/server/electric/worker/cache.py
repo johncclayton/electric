4ceb66712a48098aa7451e5af83641ca99ee6617
TheBum <35708215+TheBum@users.noreply.github.com>
2018-02-12 23:49:33 +0000

    Added case fan run state

--- src/server/electric/worker/cache.py
+++ src/server/electric/worker/cache.py
@@ -8,6 +8,7 @@ class Cache(object):
     def __init__(self):
         self.device_info = None
         self.channels = None
+        self.case_fan_running = False
         self.reset()
 
     def reset(self):
@@ -15,6 +16,7 @@ class Cache(object):
         self.channels = [
             ChannelStatus(), ChannelStatus()
         ]
+        self.case_fan_running = False
         logger.info("cache values have been reset")
 
     def get_device_info(self):
@@ -33,5 +35,11 @@ class Cache(object):
         assert (channel == 0 or channel == 1)
         self.channels[channel] = status
 
-
+    def get_case_fan_run_state(self):
+        return self.case_fan_running
+    
+    def set_case_fan_run_state(self, on_off):
+        assert(on_off == False or on_off == True)
+        self.case_fan_running = on_off
+        
 values = Cache()
