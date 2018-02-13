7aa75c9c5879cdaa424fc6938adea65a60687f01
TheBum <35708215+TheBum@users.noreply.github.com>
2018-02-13 22:06:08 +0000

    Added creation of /opt/prefs

--- src/server/run_zmq_worker.sh
+++ src/server/run_zmq_worker.sh
@@ -3,4 +3,5 @@ BASHSOURCE=$0
 DIR="$( cd "$( dirname "$BASHSOURCE" )" && pwd )"
 export PYTHONPATH=$DIR
 #watchmedo auto-restart -p "*.py;" --recursive python electric/worker/worker.py
+[ ! -d /opt/prefs ] && mkdir -p /opt/prefs
 python electric/worker/worker.py
