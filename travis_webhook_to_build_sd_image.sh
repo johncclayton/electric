a10b3037585ad1ef1f91236bf8ccdd7026a82a1a
Neil Clayton <neil@shinywhitebox.com>
2018-02-28 12:03:23 +1300

    - oh come ONNNNN

--- travis_webhook_to_build_sd_image.sh
+++ travis_webhook_to_build_sd_image.sh
@@ -1,5 +1,5 @@
 echo "The build number is: $TRAVIS_BUILD_NUMBER"
-openssl aes-256-cbc -K $encrypted_00ede72e53d7_key -iv $encrypted_00ede72e53d7_iv -in travis_rsa.pem.enc -out ./travis_rsa.pem -d 
-chmod 0600 travis_rsa.pem 
-ssh -o StrictHostKeyChecking=no -i ./travis_rsa.pem builder@coderage-software.com "cd /home/builder/electric/src/sd-image && git pull && ./create-image.sh" 
+openssl aes-256-cbc -K $encrypted_00ede72e53d7_key -iv $encrypted_00ede72e53d7_iv -in travis_rsa.pem.enc -out ./travis_rsa.pem -d
+chmod 0600 travis_rsa.pem
+ssh -o StrictHostKeyChecking=no -i ./travis_rsa.pem builder@coderage-software.com "TRAVIS_BUILD_NUMBER=$TRAVIS_BUILD_NUMBER cd /home/builder/electric/src/sd-image && git pull && ./create-image.sh"
 
