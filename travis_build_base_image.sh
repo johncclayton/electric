b4274436b910fffd873a17a5362b546606e80e84
Neil Clayton <neil@shinywhitebox.com>
2018-02-28 11:37:09 +1300

    - Fix scripts

--- travis_build_base_image.sh
+++ travis_build_base_image.sh
@@ -1,6 +1,12 @@
-docker run --rm --privileged multiarch/qemu-user-static:register --reset
-docker login -u="$DOCKER_JC_USER" -p="$DOCKER_JC_PASS"
-TAG=$TRAVIS_BUILD_NUMBER
-docker build -f docker/arm/Dockerfile-$SERVICE -t johncclayton/electric-pi-$SERVICE .
-docker tag johncclayton/electric-pi-$SERVICE johncclayton/electric-pi-$SERVICE:$TAG
-docker push johncclayton/electric-pi-$SERVICE
+echo "The build number is: $TRAVIS_BUILD_NUMBER"
+echo "The docker user is: $DOCKER_JC_USER"
+
+# OK. Get the requirements file from which the image is built.
+# Test to see if it's changed.
+
+echo "Making this FAST..."
+echo docker run --rm --privileged multiarch/qemu-user-static:register --reset
+echo docker login -u="$DOCKER_JC_USER" -p="$DOCKER_JC_PASS"
+echo docker build -f docker/arm/Dockerfile-base -t johncclayton/electric-pi-base .
+echo docker push johncclayton/electric-pi-base
+
