2cd2373647732e39c99e07c8c38eef39a2df61ca
John Clayton <john_clayton@me.com>
2018-04-15 09:28:56 +0200

    cherry picked the change to use TRAVIS_BRANCH

--- travis_webhook_to_build_sd_image.sh
+++ travis_webhook_to_build_sd_image.sh
@@ -1,5 +1,5 @@
 echo "The build number is: $TRAVIS_BUILD_NUMBER"
 openssl aes-256-cbc -K $encrypted_00ede72e53d7_key -iv $encrypted_00ede72e53d7_iv -in travis_rsa.pem.enc -out ./travis_rsa.pem -d
 chmod 0600 travis_rsa.pem
-ssh -o StrictHostKeyChecking=no -o SendEnv=TRAVIS_BUILD_NUMBER -i ./travis_rsa.pem builder@coderage-software.com "cd /home/builder/electric/src/sd-image && git pull && ./create-image.sh"
+ssh -o StrictHostKeyChecking=no -o SendEnv=TRAVIS_BUILD_NUMBER -o SendEnv=TRAVIS_BRANCH -i ./travis_rsa.pem builder@coderage-software.com "cd /home/builder/electric/src/sd-image && git reset --hard HEAD && git checkout -f $TRAVIS_BRANCH && ./create-image.sh"
 
