3e5435104f9994757fe298d232db4a938b3d229c
Neil Clayton <neil@shinywhitebox.com>
2018-08-08 09:40:29 +1200

    adding in some utils

--- src/electric-app/src/app/app.component.ts
+++ src/electric-app/src/app/app.component.ts
@@ -1,26 +1,26 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import {Platform} from '@ionic/angular';
+import {SplashScreen} from '@ionic-native/splash-screen/ngx';
+import {StatusBar} from '@ionic-native/status-bar/ngx';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html'
+    selector: 'app-root',
+    templateUrl: 'app.component.html'
 })
 export class AppComponent {
-  constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
-  ) {
-    this.initializeApp();
-  }
+    constructor(
+        private platform: Platform,
+        private splashScreen: SplashScreen,
+        private statusBar: StatusBar
+    ) {
+        this.initializeApp();
+    }
 
-  initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
-    });
-  }
+    initializeApp() {
+        this.platform.ready().then(() => {
+            this.statusBar.styleDefault();
+            this.splashScreen.hide();
+        });
+    }
 }
---
+++ src/electric-app/src/app/utils/utils.module.ts
@@ -0,0 +1,23 @@
+import {NgModule} from "@angular/core";
+import {DurationPipe, KeysPipe, ReversePipe, TempPipe} from "./pipes";
+import {StepStateDirective} from "./step-state-directive";
+
+@NgModule({
+    declarations: [
+        KeysPipe,
+        ReversePipe,
+        TempPipe,
+        DurationPipe,
+        StepStateDirective,
+    ],
+    exports: [
+        KeysPipe,
+        ReversePipe,
+        TempPipe,
+        DurationPipe,
+        StepStateDirective,
+    ]
+})
+
+export class UtilsModule {
+}
