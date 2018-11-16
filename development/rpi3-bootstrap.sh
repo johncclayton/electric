3cbb29581196ecc2ba6f8303470d4cb57274edeb
John Clayton <johnclayton72@gmail.com>
2018-11-16 14:47:32 +0100

    Added exec bits to some scripts

--- development/rpi3-bootstrap.sh
+++ development/rpi3-bootstrap.sh
@@ -8,6 +8,7 @@
 # TODO: upgrades - how do these work in dev? 
 # TODO: upgrades - how do these work in production?  presently using /LAST_VERSION 
 
+
 T=/tmp/electric-bootstrap
 
 if [ ! -d $T ]; then
