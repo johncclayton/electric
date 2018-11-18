718a37ff152eb1795cf1b3c4ce6a8e4fac0a4b48
John Clayton <johnc@filewave.com>
2018-11-18 17:15:55 +0100

    this might even make it build

--- travis_webhook_to_build_sd_image.sh
+++ travis_webhook_to_build_sd_image.sh
@@ -1,4 +1,11 @@
 echo "The build number is: $TRAVIS_BUILD_NUMBER"
+echo "The branch is: $TRAVIS_BRANCH" 
+
+if [ "unified-server" -eq "$TRAVIS_BRANCH" ]; then
+	echo "Not building for other branches at this point"
+	exit 5
+fi
+
 openssl aes-256-cbc -K $encrypted_00ede72e53d7_key -iv $encrypted_00ede72e53d7_iv -in travis_rsa.pem.enc -out ./travis_rsa.pem -d
 chmod 0600 travis_rsa.pem
 
@@ -6,6 +13,7 @@ ssh -o StrictHostKeyChecking=no \
     -o SendEnv=TRAVIS_BUILD_NUMBER \
     -o SendEnv=TRAVIS_BRANCH \
     -i ./travis_rsa.pem \
-    builder@coderage-software.com \
-    "cd /home/builder/electric/sd-image && git reset --hard HEAD && git checkout -f $TRAVIS_BRANCH && BRANCH=$TRAVIS_BRANCH ./create-image.sh"
+    -p 4545 \
+    builder@d1813.dyndns.org \
+    "cd /buildkit/electric && git reset --hard HEAD && git checkout -f $TRAVIS_BRANCH && BRANCH=$TRAVIS_BRANCH ./create-image.sh"
 
