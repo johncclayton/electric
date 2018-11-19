8d3fa8b7cd2a96893ba4801820204047e6037313
John Clayton <johnclayton72@gmail.com>
2018-11-19 11:59:59 +0100

    Remove some pointless code.

--- travis_build_sd_image.sh
+++ travis_build_sd_image.sh
@@ -1,11 +1,6 @@
 echo "The build number is: $TRAVIS_BUILD_NUMBER"
 echo "The branch is: $TRAVIS_BRANCH" 
 
-if [ "unified-server" != "$TRAVIS_BRANCH" ]; then
-	echo "Not building for other branches at this point"
-	exit 5
-fi
-
 # decode the key, using secrets embedded in the travis account
 openssl aes-256-cbc -K $encrypted_00ede72e53d7_key -iv $encrypted_00ede72e53d7_iv -in travis_rsa.pem.enc -out ./travis_rsa.pem -d
 chmod 0600 travis_rsa.pem
