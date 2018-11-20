72515c456c79de38567b8c4948ac45a06840e824
John Clayton <johnclayton72@gmail.com>
2018-11-20 20:36:09 +0100

    stop the build for a bit, until i fix it

--- travis_build_sd_image.sh
+++ travis_build_sd_image.sh
@@ -5,6 +5,9 @@ echo "The branch is: $TRAVIS_BRANCH"
 openssl aes-256-cbc -K $encrypted_00ede72e53d7_key -iv $encrypted_00ede72e53d7_iv -in travis_rsa.pem.enc -out ./travis_rsa.pem -d
 chmod 0600 travis_rsa.pem
 
+# break this until I'm done.
+exit 5
+
 ssh -o StrictHostKeyChecking=no \
     -o SendEnv=TRAVIS_BUILD_NUMBER \
     -o SendEnv=TRAVIS_BRANCH \
